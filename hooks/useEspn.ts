55d46c05adfcc37d4ad3d0d6bdd90e7bf5160106
Justin Sunseri <jmsunseri@gmail.com>
2020-10-04 03:01:36 +0800

    implementing client side caching and calling two different endpoints

--- hooks/useEspn.ts
+++ hooks/useEspn.ts
@@ -1,7 +1,7 @@
-import { IResult } from '../pages/api/stats';
 import { useState, useEffect } from 'react';
-import dayjs from 'dayjs';
 import useSWR from 'swr';
+import { IResult, Team, WeeklyResult } from '@api/models';
+import { getCurrentWeek } from '@api/utils';
 
 export interface IUseEspnResult {
   result: IResult;
@@ -14,15 +14,46 @@ const fetcher = (url: string) => fetch(url).then((r) => r.json());
 const useEspn: UseEspnHook = () => {
   const [result, setResult] = useState<IResult>({ teams: [] });
 
-  const { data, error } = useSWR<IResult>('/api/stats', fetcher, {
-    refreshInterval: 15000,
+  const { data: fullRefresh, isValidating: fullRefreshLoading } = useSWR<
+    IResult
+  >('/api/stats/all', fetcher, {
+    refreshInterval: 86400000,
   });
 
-  useEffect(() => {
-    if (data) {
-      setResult({ ...data, updatedDate: dayjs(data.updatedDate) });
+  const { data: refreshed } = useSWR<IResult>(
+    '/api/stats/this-week',
+    (url: string) => {
+      if (fullRefresh && !fullRefreshLoading) {
+        return fetch(url).then((r) => r.json());
+      }
+    },
+    {
+      refreshInterval: 15000,
     }
-  }, [data]);
+  );
+
+  useEffect(() => {
+    const weekId = getCurrentWeek();
+
+    if (refreshed) {
+      const teams = refreshed.teams.map(
+        (team: Team): Team => ({
+          ...team,
+          weeklyResults: [
+            ...team.weeklyResults,
+            ...fullRefresh.teams
+              .find((ct: Team) => ct.id === team.id)
+              .weeklyResults.filter((wr: WeeklyResult) => wr.weekId !== weekId),
+          ],
+        })
+      );
+      setResult((previous: IResult) => ({ ...previous, ...refreshed, teams }));
+    }
+  }, [refreshed]);
+
+  useEffect(() => {
+    setResult((previous: IResult) => ({ ...previous, ...fullRefresh }));
+  }, [fullRefresh]);
 
   return { result };
 };
